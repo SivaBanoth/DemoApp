7b66dc0a9a30e59be85e69dd2e22909331167401
SivaBanoth <siva.banoth.95@gmail.com>
2023-12-28 16:11:48 +0530

    First Commit

---
+++ DemoApp/src/in/ineuron/main/DemoApp.java
@@ -0,0 +1,20 @@
+package in.ineuron.main;
+
+public class DemoApp {
+
+	public static void main(String[] args) {
+		
+		int[] a = {10,20,30,40};
+		
+		for(int x : a) {
+		
+			System.out.println(x);
+		}
+		
+		String name = "sachin";
+		int count = name.toUpperCase().length();
+		System.out.println("The lenght of the String is : " + count);
+		
+	}
+
+}
